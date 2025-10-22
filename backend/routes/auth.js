625a6ab428e87dfd3c8ff78edc7c5d9f7d465d7a
iSitanshu <sitanshumishra18@gmail.com>
2025-10-22 10:46:44 +0530

    auth setup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { signinSchema } from "./../types.js";
+import { signinSchema, signupSchema } from "./../types.js";
+import bcrypt from 'bcrypt';
 
 const router = Router();
 
@@ -9,7 +10,19 @@ router.post('/login', async (req, res) => {
     if(!success) {
         return res.status(400).json({message: "Invalid Request", error: data})
     }
-    
+    // check the password with the encrypted password and return the token if matches
+})
+
+router.post('/signup', async (req, res) => {
+    const { success, data } = signupSchema.safeParse(req.body);
+    if(!success) {
+        return res.status(400).json({message: "Invalid Request", error: data})
+    } 
+    // bcrypt to encrypt the password
+    bcrypt.hash(data.password, process.env.SALT_ROUNDS || 10, (err, hashpassword) => {
+        console.log(hashpassword)
+    })
+    res.status(200).json({ message: "Signup successful"});
 })
 
 export default router;
