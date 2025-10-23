0e32b0a77d170c8a275f3a17bb563bd8228fd832
iSitanshu <sitanshumishra18@gmail.com>
2025-10-23 10:50:37 +0530

    adding cloudinary to the project

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,19 @@ import authRouter from "./routes/auth.js"
 import razorpayRouter from "./routes/razorpay.js"
 import newsletterRouter from "./routes/newsletter.js"
 import consultancyRouter from './routes/consultancy.js'
+import cloudinary from 'cloudinary'
 
 const app = express();
 const PORT = process.env.PORT || 69;
 
 app.use(cors());
 app.use(express.json());
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+  secure: true // Recommended for HTTPS URLs
+});
 
 app.use("/api/auth", authRouter);
 app.use("/api/payment", razorpayRouter);
