644946c37182a2e24eaeb64a4c6425dd5b938e94
iSitanshu <Sitanshumishra18@gmail.com>
2025-10-26 20:20:14 +0530

    resolve the postgres issue in the current which get resolve by deleting prisma.config.ts file in the rot folder

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// import express from "express"
-// import cors from "cors"
-// import authRouter from "./routes/auth.js"
-// import razorpayRouter from "./routes/razorpay.js"
-// import newsletterRouter from "./routes/newsletter.js"
-// import consultancyRouter from './routes/consultancy.js'
-// import networkRouter from './routes/network.js'
-// import blogRouter from './routes/blogs.js'
-// import dashboardRouter from './routes/dashboard.js'
+import express from "express"
+import cors from "cors"
+import authRouter from "./routes/auth.js"
+import razorpayRouter from "./routes/razorpay.js"
+import newsletterRouter from "./routes/newsletter.js"
+import consultancyRouter from './routes/consultancy.js'
+import networkRouter from './routes/network.js'
+import blogRouter from './routes/blogs.js'
+import dashboardRouter from './routes/dashboard.js'
 
 const app = express();
 const PORT = process.env.PORT || 69;
@@ -19,13 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));   
 
 
-// app.use("/api/auth", authRouter);
-// app.use("/api/payment", razorpayRouter);
-// app.use("/api/newsletter", newsletterRouter);
-// app.use("/api/consultancy", consultancyRouter)
-// app.use('/api/network', networkRouter);
-// app.use('/api/blogs', blogRouter);
-// app.use('/api/dashboard', dashboardRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/payment", razorpayRouter);
+app.use("/api/newsletter", newsletterRouter);
+app.use("/api/consultancy", consultancyRouter)
+app.use('/api/network', networkRouter);
+app.use('/api/blogs', blogRouter);
+app.use('/api/dashboard', dashboardRouter);
 
 app.listen(PORT, () => {
    console.log(`Server is running on PORT: ${PORT}`);
